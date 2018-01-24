ebc4946b8998ce9b74225d6a34cfe6126066a7ce
Charles Basenga Kiyanda <personal@charleskiyanda.com>
2018-01-24 13:51:49 -0500

    restoring bno specific code in cube_test.py

--- cube_test.py
+++ cube_test.py
@@ -7,36 +7,36 @@ import logging
 import time
 
 from operator import itemgetter
-#####from Adafruit_BNO055 import BNO055
+from Adafruit_BNO055 import BNO055
 
 # Create and configure the BNO sensor connection
 # Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
-#####bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
+bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
 
 # Enable verbose debug logging if -v is passed as a parameter.
-#####if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-#####    logging.basicConfig(level=logging.DEBUG)
+if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+    logging.basicConfig(level=logging.DEBUG)
 
 # Initialize the BNO055 and stop if something went wrong.
-#####if not bno.begin():
-#####    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+if not bno.begin():
+    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
 # Print system status and self test result.
-#####status, self_test, error = bno.get_system_status()
-#####print('System status: {0}'.format(status))
-#####print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+status, self_test, error = bno.get_system_status()
+print('System status: {0}'.format(status))
+print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
 # Print out an error if system status is in error mode.
-#####if status == 0x01:
-#####    print('System error: {0}'.format(error))
-#####    print('See datasheet section 4.3.59 for the meaning.')
+if status == 0x01:
+    print('System error: {0}'.format(error))
+    print('See datasheet section 4.3.59 for the meaning.')
 
 # Print BNO055 software revision and other diagnostic data.
-#####sw, bl, accel, mag, gyro = bno.get_revision()
-#####print('Software version:   {0}'.format(sw))
-#####print('Bootloader version: {0}'.format(bl))
-#####print('Accelerometer ID:   0x{0:02X}'.format(accel))
-#####print('Magnetometer ID:    0x{0:02X}'.format(mag))
-#####print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+sw, bl, accel, mag, gyro = bno.get_revision()
+print('Software version:   {0}'.format(sw))
+print('Bootloader version: {0}'.format(bl))
+print('Accelerometer ID:   0x{0:02X}'.format(accel))
+print('Magnetometer ID:    0x{0:02X}'.format(mag))
+print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
 class Point3D:
     def __init__(self, x = 0, y = 0, z = 0):
@@ -174,8 +174,8 @@ class Simulation:
                              (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
                 pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
                 
-            #####self.heading, self.roll, self.pitch = bno.read_euler()
-            #####self.xaccel, self.yaccel, self.zaccel = bno.read_linear_acceleration()
+            self.heading, self.roll, self.pitch = bno.read_euler()
+            self.xaccel, self.yaccel, self.zaccel = bno.read_linear_acceleration()
             self.current_time = time.time()
             self.heading = -self.heading
             self.pitch   = -self.pitch
