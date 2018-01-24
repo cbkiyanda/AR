981bdef4336b52cfa1d0c7b41f5375f80e28b69b
Charles Basenga Kiyanda <personal@charleskiyanda.com>
2018-01-23 22:10:59 -0500

    Removing BNO055 specific code in cube test code

--- Translation of a cube.py
+++ Translation of a cube.py
@@ -7,36 +7,36 @@ import logging
 import time
 
 from operator import itemgetter
-from Adafruit_BNO055 import BNO055
+#####from Adafruit_BNO055 import BNO055
 
 # Create and configure the BNO sensor connection
 # Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
-bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
+#####bno = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
 
 # Enable verbose debug logging if -v is passed as a parameter.
-if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-    logging.basicConfig(level=logging.DEBUG)
+#####if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+#####    logging.basicConfig(level=logging.DEBUG)
 
 # Initialize the BNO055 and stop if something went wrong.
-if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+#####if not bno.begin():
+#####    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
 # Print system status and self test result.
-status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+#####status, self_test, error = bno.get_system_status()
+#####print('System status: {0}'.format(status))
+#####print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
 # Print out an error if system status is in error mode.
-if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
+#####if status == 0x01:
+#####    print('System error: {0}'.format(error))
+#####    print('See datasheet section 4.3.59 for the meaning.')
 
 # Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+#####sw, bl, accel, mag, gyro = bno.get_revision()
+#####print('Software version:   {0}'.format(sw))
+#####print('Bootloader version: {0}'.format(bl))
+#####print('Accelerometer ID:   0x{0:02X}'.format(accel))
+#####print('Magnetometer ID:    0x{0:02X}'.format(mag))
+#####print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
 class Point3D:
     def __init__(self, x = 0, y = 0, z = 0):
@@ -101,14 +101,14 @@ class Simulation:
         self.clock = pygame.time.Clock()
 
         self.vertices = [
-            Point3D(-1,1,40),
-            Point3D(1,1,40),
-            Point3D(1,-1,40),
-            Point3D(-1,-1,40),
-            Point3D(-1,1,42),
-            Point3D(1,1,42),
-            Point3D(1,-1,42),
-            Point3D(-1,-1,42)
+            Point3D(-1, 1, 40),
+            Point3D( 1, 1, 40),
+            Point3D( 1,-1, 40),
+            Point3D(-1,-1, 40),
+            Point3D(-1, 1, 42),
+            Point3D( 1, 1, 42),
+            Point3D( 1,-1, 42),
+            Point3D(-1,-1, 42)
         ]
         # Define the vertices that compose each of the 6 faces. These numbers are
         # indices to the vertices list defined above.
@@ -147,7 +147,7 @@ class Simulation:
             t = []
             
             for v in self.vertices:
-                # Translate the point about X axis, then about Y axis, and finally about Z axis. (No rotation ,only translation)
+                # Translate the point about X axis, then about Y axis, and finally about Z axis.
                 r = v.rotateX(self.angle).rotateY(self.angle).rotateZ(self.angle).translateX(self.xdist).translateY(self.ydist).translateZ(self.zdist)
                          
                # Transform the point from 3D to 2D
@@ -173,31 +173,30 @@ class Simulation:
                              (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
                              (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
                 pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
-            
-            self.heading, self.roll, self.pitch = bno.read_euler()
-            self.xaccel, self.yaccel, self.zaccel = bno.read_linear_acceleration()
-            #self.xaccel, self.yaccel, self.zaccel = bno.read_accelerometer()
-	    self.current_time = time.time()
+                
+            #####self.heading, self.roll, self.pitch = bno.read_euler()
+            #####self.xaccel, self.yaccel, self.zaccel = bno.read_linear_acceleration()
+            self.current_time = time.time()
             self.heading = -self.heading
             self.pitch   = -self.pitch
-	    #print(self.heading)
- 	    #print(self.pitch)
-	    #print(self.roll)
-	    print("accelerations, x, y, z")
-	    #print(self.xaccel)
-	    #print(self.yaccel)
-	    print(self.zaccel)
-            print(self.zvel)
-            print(self.zdist)
-	    print("time since previous measurement")
+            #print(self.heading)
+       	    #print(self.pitch)
+            #print(self.roll)
+            #print("accelerations, x, y, z")
+            #print(self.xaccel)
+            #print(self.yaccel)
+            #print(self.zaccel)
+            #print(self.zvel)
+            #print(self.zdist)
+            print("time since previous measurement")
             print(self.current_time - self.previous_time)
             print(self.current_time)
-	    
-            self.angle += 0
+            
+            self.angle += 2
             #self.zdist += 0
             #self.ydist += 0
             #self.xdist += 0
-     
+           
             dt = (self.current_time - self.previous_time)	    
 
             self.zdist   = self.zdist + self.zvel*dt + 0.5*(self.zaccel)*dt**2
@@ -208,8 +207,10 @@ class Simulation:
 
             self.xdist   = self.xdist + self.xvel*dt + 0.5*(self.xaccel)*dt**2
             self.xvel    = self.xvel + self.xaccel*dt
-   
-         pygame.display.flip()
-	    self.previous_time = self.current_time
+         
+            pygame.display.flip()
+            self.previous_time = self.current_time
+
+
 if __name__ == "__main__":
     Simulation().run()
